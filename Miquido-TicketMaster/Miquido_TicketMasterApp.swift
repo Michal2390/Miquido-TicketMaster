b00dc2e5c436ae801e43e310f800c8be746f8392
Michał Fereniec <michalkoks99@gmail.com>
2024-11-03 16:32:32 +0100

    Initial Commit

---
+++ Miquido-TicketMaster/Miquido_TicketMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Miquido_TicketMasterApp.swift
+//  Miquido-TicketMaster
+//
+//  Created by Michal Fereniec on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Miquido_TicketMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
